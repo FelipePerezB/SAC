b6b2d64f84471a926e6b289860f12bb062893b2f
= <=>
2022-10-01 21:14:10 -0300

    V1.2.2

--- src/layouts/GameLayout.jsx
+++ src/layouts/GameLayout.jsx
@@ -5,25 +5,29 @@ import Layout from "./Layout";
 import ProgressVar from "@components/ProgressVar";
 
 const GameLayout = ({ children, settings, type }) => {
-  const [layout, setLayout] = useState("")
-  useEffect(()=>{
-    if(settings.isFinalGame){
-      console.log("A")
-      setLayout("layout-without-ui")
-    } else if(type!=="tutorial"){
-      setLayout("layout-without-nav")
-    } else{
-      setLayout("tutorial")
+  const [layout, setLayout] = useState("");
+  useEffect(() => {
+    if (settings.isFinalGame) {
+      console.log("A");
+      setLayout("layout-without-ui");
+    } else if (type !== "tutorial") {
+      setLayout("layout-without-nav");
+    } else {
+      setLayout("tutorial");
     }
-  }, [settings])
+  }, [settings]);
   return (
     <>
       <Layout type={layout}>
         {settings.isFinalGame ? (
-          <Victory title={"LEVEL COMPLETED!"} stats={settings.stats} id={settings.levelId} />
+          <Victory
+            title={"LEVEL COMPLETED!"}
+            stats={settings.stats}
+            id={settings.levelId}
+          />
         ) : (
           <div className={styles["game-container"]}>
-            <ProgressVar type={"levels"} progress={settings.progress}/>
+            <ProgressVar type={"levels"} progress={settings.progress} />
             {children}
           </div>
         )}
